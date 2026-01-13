2b9d91e36ac1e9786d84a436cc2341d4413782c5
rw1030 <rw1030@student.ubc.ca>
2026-01-12 21:26:35 -0800

    add tes_whitespace func

--- tests/unit/test_clean_date.py
+++ tests/unit/test_clean_date.py
@@ -1,1 +1,6 @@
 from canadata_clean.clean_date import clean_date
+
+def test_whitespace():
+    out = clean_date(" 1991-10-20")
+    expected_out = "1991-10-20"
+    assert  out == expected_out, f"Expected {expected_out} but got {out}"
